bbbab0b6e944bfc3da9da6633a16bcb3413fa0c8
agniksarkar1107 <121817949+agniksarkar1107@users.noreply.github.com>
2024-02-01 17:26:21 +0530

    Add files via upload

---
+++ DataScienceCoursera.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# In this notebook,data science tools are summarized.
+
+# Some languages are:
+# 1)Python
+# 2)R
+# 3)SQL
+# 4)C++
+# 5)Java
+
+# Some Libraries are:
+# 1)TensorFlow
+# 2)NumPy
+# 3)SciPy
+# 4)Pandas
+# 5)Matplotlib
+# 6)Keras
+# 7)Scikit-Learn
+# 8)PyTorch
+# 
+
+# |Data Science Tools|
+# |---|
+# |SAS|
+# |Apache Hadoop|
+# |TensoFlow|
+
+# # Below are a few examples of evaluating arithmetic expressions in python
+
+# In[2]:
+
+
+(3*4)+5
+
+
+# In[3]:
+
+
+200/60
+
+
+# # Objective
+
+# * List popular languages for data science
+# * List popular libraries for data science
+# * List of well known data science tools
+# * How to provide comments in the cell
+# 
+
+# # Author's name- Agnik Sarkar
+
+# In[ ]:
+
+
+
+
